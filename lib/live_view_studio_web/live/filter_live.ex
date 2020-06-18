8cce648183eed774370c854b90f1d58a4912495c
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2020-06-18 16:51:45 -0300

    Complete filter exercise

---
+++ lib/live_view_studio_web/live/filter_live.ex
@@ -0,0 +1,104 @@
+defmodule LiveViewStudioWeb.FilterLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Boats
+
+  def mount(_params, _session, socket) do
+    socket =
+      assign(socket,
+        boats: Boats.list_boats(),
+        type: "",
+        prices: []
+      )
+
+    {:ok, socket, temporary_assigns: [boats: []]}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Daily Boat Rentals</h1>
+    <div id="filter">
+      <form phx-change="filter">
+        <div class="filters">
+          <select name="type">
+            <%= options_for_select(type_options(), @type) %>
+          </select>
+          <div class="prices">
+            <input type="hidden" name="prices[]" value=""/>
+            <%= for price <- ["$", "$$", "$$$"] do %>
+              <%= price_checkbox(price: price, checked: price in @prices) %>
+            <% end %>
+          </div>
+          <a href="#" phx-click="clear">Clear all</a>
+        </div>
+      </form>
+    <div class="boats">
+    <%= for boat <- @boats do %>
+      <div class="card">
+        <img src="<%= boat.image %>">
+        <div class="content">
+          <div class="model">
+            <%= boat.model %>
+          </div>
+          <div class="details">
+            <span class="price">
+              <%= boat.price %>
+            </span>
+            <span class="type">
+              <%= boat.type %>
+            </span>
+          </div>
+        </div>
+      </div>
+    <% end %>
+    </div>
+    </div>
+    """
+  end
+
+  def handle_event("filter", %{"type" => type, "prices" => prices}, socket) do
+    boats = Boats.list_boats(type: type, prices: prices)
+
+    socket =
+      assign(socket,
+        type: type,
+        prices: prices,
+        boats: boats
+      )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("clear", _, socket) do
+    socket =
+      assign(socket,
+        boats: Boats.list_boats(),
+        type: "",
+        prices: []
+      )
+    {:noreply, socket}
+  end
+
+  defp type_options do
+    [
+      "All Types": "",
+      Fishing: "fishing",
+      Sporting: "sporting",
+      Sailing: "sailing"
+    ]
+  end
+
+  defp price_checkbox(assigns) do
+    assigns = Enum.into(assigns, %{})
+
+    ~L"""
+    <input type="checkbox"
+          id="<%= @price %>"
+          name="prices[]"
+          value="<%= @price %>"
+          <%= if @checked, do: "checked" %>
+    />
+    <label for="<%= @price %>"><%= @price %></label>
+    """
+  end
+end
